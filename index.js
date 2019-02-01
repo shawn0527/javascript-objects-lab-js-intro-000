0973b7409b23684f9891cff19855d39b17c02271
Shawn Ren <doggy-ren@hotmail.com>
2019-02-01 18:47:17 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
 var recipes={}
 
+var obj={prop1:1}
 function updateObjectWithKeyAndValue(object,key,value){
   return newObj=Object.assign({}, object, {[key]:value})
 }
+updateObjectWithKeyAndValue(obj,'prop2',2)
