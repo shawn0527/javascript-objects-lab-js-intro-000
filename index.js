c2ae4b320e792cdc6a9b33f16543554cb06a0181
Shawn Ren <doggy-ren@hotmail.com>
2019-02-01 18:59:29 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -5,3 +5,7 @@ function updateObjectWithKeyAndValue(object,key,value){
 }
 var obj={prop:1}
 updateObjectWithKeyAndValue(obj,'prop2',2)
+
+function destructivelyUpdateObjectWithKeyAndValue(object,key,value){
+  object[key]=value
+}
